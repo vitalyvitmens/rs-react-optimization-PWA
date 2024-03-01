a159457cbeb62e935c35e827c3ef90de04c64d0a
vitalyvitmens <otm_@tut.by>
2024-03-01 14:41:27 +0300

    chore(src\index.js): меняет путь регистрации serviceWorker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from './App'
 
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-		.register('/public/sw.js')
+		.register('./sw.js')
 		.then((reg) => console.log('Service Worker registered!', reg))
 		.catch((err) => console.log('Service Worker not registered!', err))
 }
