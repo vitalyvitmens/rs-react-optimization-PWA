2b18b782905add928b39151ef79c3fc837fad9b2
vitalyvitmens <otm_@tut.by>
2024-03-01 14:30:21 +0300

    chore(src\index.js): регистрирует serviceWorker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
 
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/public/sw.js')
+		.then((reg) => console.log('Service Worker registered!', reg))
+		.catch((err) => console.log('Service Worker not registered!', err))
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<App />)
