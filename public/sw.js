2346b1adf26fcbf994fab458a3e11d4d3890a89c
vitalyvitmens <otm_@tut.by>
2024-03-01 16:33:50 +0300

    fix: настраивает режим offline

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,19 @@
-const staticCacheName = 'static-site-v1'
-const dynamicCacheName = 'dynamic-site-v1'
+const staticCacheName = 'static-site'
+// const dynamicCacheName = 'dynamic-site-v1'
 
 const ASSETS = [
 	'/',
-	'/public/index.html',
-	'/src/index.css',
-	'/src/app.module.css',
-	'/src/index.js',
-	'/src/App.js',
+	'index.html',
+	// '/bundle.js',
+	// '/public/favicon.jpg',
+	// '/public/assets/icons/icon-144x144.png',
+	// '/public/assets/screenshots/screenshot-1900x920.png',
+
+	// '/index.html',
+	'src/index.css',
+	'src/app.module.css',
+	'src/index.js',
+	'src/App.js',
 ]
 
 // install event
@@ -17,46 +23,82 @@ self.addEventListener('install', async (event) => {
 	await cache.addAll(ASSETS)
 })
 
-// activated event
+// activate event
 // eslint-disable-next-line no-restricted-globals
-self.addEventListener('activate', async (event) => {
-	const cachesKeysArr = await caches.keys()
-	await Promise.all(
-		cachesKeysArr
-			.filter((key) => key !== staticCacheName && key !== dynamicCacheName)
-			.map((key) => caches.delete(key))
-	)
+self.addEventListener('activate', (event) => {
+	console.log('####: Service Worker has been activated')
 })
 
-// fetch event
+// fetch
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener('fetch', (event) => {
-	event.respondWith(cacheFirst(event.request))
+	event.respondWith(
+		caches.match(event.request).then((cacheRes) => {
+			return cacheRes || fetch(event.request)
+		})
+	)
+	console.log('####: fetch', event)
 })
 
-async function cacheFirst(request) {
-	const cached = await caches.match(request)
-	try {
-		return (
-			cached ??
-			(await fetch(request).then((response) => {
-				return networkFirst(request)
-			}))
-		)
-	} catch (error) {
-		return networkFirst(request)
-	}
-}
+// const staticCacheName = 'static-site'
+// const dynamicCacheName = 'dynamic-site'
 
-async function networkFirst(request) {
-	const cache = await caches.open(dynamicCacheName)
-	try {
-		const response = await fetch(request)
-		await cache.put(request, response.clone())
-		return response
-	} catch (error) {
-		console.log('####: error', error)
-		const cached = await cache.match(request)
-		return cached ?? (await caches.match('/src/pages/NotFound.jsx'))
-	}
-}
+// const ASSETS = [
+// '/',
+// '/index.html',
+// '/src/index.css',
+// '/src/app.module.css',
+// '/src/index.js',
+// '/src/App.js',
+// ]
+
+// // install event
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('install', async (event) => {
+// 	const cache = await caches.open(staticCacheName)
+// 	await cache.addAll(ASSETS)
+// })
+
+// // activated event
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('activate', async (event) => {
+// 	const cachesKeysArr = await caches.keys()
+// 	await Promise.all(
+// 		cachesKeysArr
+// 			.filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+// 			.map((key) => caches.delete(key))
+// 	)
+// })
+
+// // fetch event
+// // eslint-disable-next-line no-restricted-globals
+// self.addEventListener('fetch', (event) => {
+// 	event.respondWith(cacheFirst(event.request))
+// })
+
+// async function cacheFirst(request) {
+// 	const cached = await caches.match(request)
+// 	try {
+// 		return (
+// 			cached ??
+// 			(await fetch(request).then((response) => {
+// 				return networkFirst(request)
+// 			}))
+// 		)
+// 	} catch (error) {
+// 		return networkFirst(request)
+// 	}
+// }
+
+// async function networkFirst(request) {
+// 	const cache = await caches.open(dynamicCacheName)
+// 	try {
+// 		const response = await fetch(request)
+// 		await cache.put(request, response.clone())
+// 		return response
+// 	} catch (error) {
+// 		console.log('####: error', error)
+// 		const cached = await cache.match(request)
+// 		return cached ?? (await caches.match('/src/pages/NotFound.jsx'))
+// 	}
+// }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App />)
+
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('./sw.js')
 		.then((reg) => console.log('Service Worker registered!', reg))
 		.catch((err) => console.log('Service Worker not registered!', err))
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(<App />)
